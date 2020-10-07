2abbbcfb6567b26184e3686de80260ec24783798
Nguyen Thanh Dong <66619015+chep267@users.noreply.github.com>
2020-10-07 08:18:56 +0700

    Create bai2.c

---
+++ bai2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int giaithua(int a) {
+    int s = 1;
+    for (int i=1; i<=a; i++) {
+        s *= i;
+    }
+    return s;
+}
+
+int main()
+{
+    int n, s = 0;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    for (int i = 1; i<=n; i++) {
+        s += giaithua(i) * pow(-1,i-1);
+    }
+    printf("Tong cac giai thua tu 1 den %d la: %d", n,s);
+    
+    return 0;
+}
