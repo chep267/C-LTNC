ccba20e9c58201a012f09701e84b34c834f6aa6a
Nguyen Thanh Dong <66619015+chep267@users.noreply.github.com>
2020-10-09 19:27:00 +0700

    Update bai12.c

--- bai12.c
+++ bai12.c
@@ -1,3 +1,5 @@
+#include "stdio.h"
+
 int Fibonacci(int n)
 {
     if (n == 1 || n == 2)
@@ -5,11 +7,30 @@ int Fibonacci(int n)
     return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
+int Fibonacci2(int n)
+{
+    int a1 = 1;
+    int a2 = 1;
+    int a = 0;
+    int i = 3;
+    
+    while (i <= n) {
+    	a = a1+a2;
+    	a1 = a2;
+    	a2 = a;
+    	i++;
+	}
+	return a;
+}
+
 int main()
 {
     int n;
-    printf("nhap n: ");
-    scanf("%d", &n);
-    printf("So Fibonacci thu %d la: %d", n, Fibonacci(n));
+    do {
+    	printf("nhap n: ");
+    	scanf("%d", &n);
+	} while (n<=0);
+    
+    printf("So Fibonacci thu %d la: %d", n, Fibonacci2(n));
     return 0;
 }
