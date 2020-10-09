bf0c3267c8c890bf9d10e544f561a291d1f4d1ef
Nguyen Thanh Dong <66619015+chep267@users.noreply.github.com>
2020-10-09 18:56:46 +0700

    Update bai10.c

--- bai10.c
+++ bai10.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
+int demchuso(int n) {
+	if (n < 0) n = -n;
+	int dem = 0;
+	while (n != 0) {
+		n = (n-n%10) / 10;
+		dem ++;
+	}
+	return dem;
+}
 int isArmstrong(int n) {
+	int d = demchuso(n);
     int m = n;
     int s = 0;
     while(m != 0) {
-        s += pow(m%10,3);
-        m = (m - m%10)/10;
+        s += pow(m%10,d);
+        m = (m - m%10) / 10;
     }
     if (s == n) return 1;
     else return 0;
@@ -18,6 +28,7 @@ int main()
     int n;
     printf("Nhap so nguyen n: ");
     scanf("%d", &n);
+    
     if (isArmstrong(n)) printf("%d la so Armstrong", n);
     else printf("%d khong phai la so Armstrong", n);
     return 0;
